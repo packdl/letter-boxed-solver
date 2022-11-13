0818c4fca5094c26ea561f88db49a20152efea77
Derik <dpack@ieee.org>
2022-11-13 13:59:57 -0500

    Major refactor with more functions

--- lbsolver/lbsolver.py
+++ lbsolver/lbsolver.py
@@ -1,5 +1,6 @@
 import pprint as pp
 from collections import defaultdict
+from collections.abc import Iterator
 from random import choice
 
 try:
@@ -22,8 +23,9 @@ game_letters = side1 + side2 + side3 + side4
 
 def get_side(letter, sides):
     for side in sides:
-        if a in side:
+        if letter in side:
             return side
+    return []
 
 
 def get_unused_letters(my_word, g_letters):
@@ -36,59 +38,87 @@ def check_for_unused_letters(word, unused_letters):
     return any(letter in unused_letters for letter in word)
 
 
-with open("/usr/share/dict/words", "r") as dictionary, open("all_words", "w") as output:
-    for word in dictionary:
-        word = word.strip()
-        if (
-            word[0].islower()
-            and len(word) >= 4
-            and all(letter in game_letters for letter in word.lower())  # type: ignore
-        ):
-            output.write(word + "\n")
+def generate_valid_words(dictionary_path: str = None) -> Iterator[str]:
+    if not dictionary_path:
+        dictionary_path = "/usr/share/dict/words"
 
-with open("all_words", "r") as reduced_list, open("valid_words", "w") as valid_file:
-    for word in reduced_list:
-        word = word.lower().strip()
-        if not word:
+        with open(dictionary_path, "r") as dictionary:
+            for word in dictionary:
+                word = word.strip()
+                if (
+                    word[0].islower()
+                    and len(word) >= 3
+                    and all(letter in game_letters for letter in word.lower())  # type: ignore
+                ):
+                    word = word.lower().strip()
+                    if not word:
+                        continue
+
+                    valid_word = is_word_valid(word)
+                    if valid_word:
+                        yield word
+                else:
+                    continue
+
+
+def is_word_valid(word):
+    valid_word = True
+    for a, b in zip(word, word[1:]):
+        if a == b:
+            valid_word = False
             break
+        side = get_side(a, [side1, side2, side3, side4])
+        if b in side:
+            valid_word = False
+            break
+    return valid_word
 
-        valid_word = True
-        for a, b in zip(word, word[1:]):
-            if a == b:
-                valid_word = False
-                break
-            side = get_side(a, [side1, side2, side3, side4])
-            # print(a, side, word)
-            if side:
-                if b in side:
-                    valid_word = False
-                    break
-        if valid_word:
-            valid_file.write(word + "\n")
 
-num_let_dict = defaultdict(list)
-words = []
-with open("valid_words", "r") as valid_file:
-    for word in valid_file:
-        word = word.strip()
-        if word:
-            num_let_dict[(len(word), word[0])].append(word)
-            words.append(word)
+words = list(generate_valid_words())
+
+
+def get_ranked_words(words) -> dict:
+    """Organized valid words into a dictionary with a tuple as a key
+
+    Key is a tuple(len(set(used letters)), first_letter_of_word) - A higher # of used letters is better ranked.
+    Value is a list of words that start with the leter identified int they key
+    """
+    num_let_dict = defaultdict(list)
+    for word in words:
+        num_letters_used = len(game_letters) - len(
+            get_unused_letters(word, game_letters)
+        )
+        num_let_dict[(num_letters_used, word[0])].append(word)
+    return num_let_dict
+
+
+num_let_dict = get_ranked_words(words)
 
 sorted_keys = sorted(num_let_dict, reverse=True)
 
 answers = []
-for word in words:
-    letters_left = get_unused_letters(word, game_letters)
-    potential_answer = []
-    potential_answer.append(word)
-    for x in range(1, max_size):
+
+
+def dfs(word: str, possible_answer: tuple):
+
+    if len(possible_answer) >= max_size:
+        return
+
+    possible_answer = possible_answer + (word,)
+    letters_left = get_unused_letters("".join(possible_answer), game_letters)
+    if letters_left:
+
         filter_keys = (key for key in sorted_keys if key[1] == word[-1])
-        key = choice(list(filter_keys))
-        word = choice(num_let_dict[key])
-        potential_answer.append(word)
-        if not get_unused_letters("".join(potential_answer), game_letters):
-            answers.append(potential_answer)
-            break
-answers.sort(key=lambda x: len(x))
-pp.pprint(answers)
+        for key in sorted(filter_keys, reverse=True):
+            for next_word in num_let_dict[key]:
+                dfs(next_word, possible_answer)
+    else:
+        answers.append(possible_answer)
+        return
+
+
+if __name__ == "__main__":
+    for key in sorted_keys:
+        for word in num_let_dict[key]:
+            dfs(word, tuple())
+    pp.pprint(answers)
